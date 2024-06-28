66ab13ec80bf73b22764d071647544adb6fcc431
F1r3f0x <plabin@outlook.cl>
2024-06-28 15:29:51 -0400

    move to func_godot, update godot version

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -1,12 +1,14 @@
 extends Node3D
 
-var map: QodotMap
+var map: FuncGodotMap
 var map_collider: StaticBody3D
+
 
 func _ready():
 	map = $Map
-	map_collider = $Map/entity_0_worldspawn
+	map_collider = $Map/entity_0_worldspawn	
 	
 	$Map/entity_0_worldspawn.set_collision_layer_value(1, false)
 	$Map/entity_0_worldspawn.set_collision_layer_value(2, true)
 	$Map/entity_0_worldspawn.set_collision_mask_value(1, false)
+
